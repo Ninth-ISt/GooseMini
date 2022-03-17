ff97cbefcffeaad1288a17945a3d39f7cdd3ac7c
KuznetsovISt-181 <73105002+Ninth-ISt@users.noreply.github.com>
2022-03-17 13:21:21 +0300

    v1

---
+++ Telegram_Kuzbot/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace Telegram_Kuzbot
+{
+    public class Program
+    {
+        public static TelegramBotClient client;
+        private static string Token { get; set; } = Settings.Token;
+
+        [Obsolete]
+        public static void Main()
+        {
+            client = new TelegramBotClient(Token);
+            client.StartReceiving();
+            client.OnMessage += BotOnMessageReceived;
+            Console.ReadLine();
+            client.StopReceiving();
+        }
+
+        [Obsolete]
+        public static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+
+            switch (message.Text)
+            {
+                case "/start":
+                    Console.WriteLine($"Пришло сообщение с текстом: {message.Text}");
+                    await client.SendTextMessageAsync(message.Chat.Id, "Приветствую тебя, странник мира под названием «Интернет»! Чем могу быть полезен? (/help - помощь)");
+                    break;
+
+                case "/lessons":
+                    Console.WriteLine($"Пришло сообщение с текстом: {message.Text}");
+                    await client.SendTextMessageAsync(message.Chat.Id, "Введите группу и желаемую дату. Формат входной строки группа:день-месяц");
+                    break;
+                case "/room":
+                    Console.WriteLine($"Пришло сообщение с текстом: {message.Text}");
+                    await client.SendTextMessageAsync(message.Chat.Id, "Введите номер аудитории и желаемую дату. Формат входной строки аудитория:дата");
+                    break;
+                case "/teacher":
+                    Console.WriteLine($"Пришло сообщение с текстом: {message.Text}");
+                    await client.SendTextMessageAsync(message.Chat.Id, "Введите преподавателя и желаемую дату. Формат входной строки полное ФИО:дата");
+                    break;
+                case "/help":
+                    Console.WriteLine($"Пришло сообщение с текстом: {message.Text}");
+                    await client.SendTextMessageAsync(message.Chat.Id, "/start — начало общения с KuzBot");
+                    break;
+            }
+        }
+    }
+}
---
+++ Telegram_Kuzbot/Settings.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telegram_Kuzbot
+{
+    class Settings
+    {
+        public static string Token { get; set; } = "5230390950:AAF3kjJ6Yux4nn2D-_yDDcqDDdNSzFAQCYA";
+    }
+}
